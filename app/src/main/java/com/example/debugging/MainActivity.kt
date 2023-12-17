f6f6fbafd82be48b8409856c3ab8e4aec7d60e72
Volodymyr Yevsiuk <vov2706@gmail.com>
2023-12-17 18:43:45 +0200

    fix bug

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -12,7 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         logging()
         division()
-        first()
     }
 
     private fun logging() {
@@ -26,28 +25,9 @@ class MainActivity : AppCompatActivity() {
     private fun division() {
         val numerator = 60
         var denominator = 4
-        repeat(5) {
+        repeat(4) {
             Log.v(TAG, "${numerator / denominator}")
             denominator--
         }
-    }
-
-    private fun first() {
-        second()
-        Log.v(TAG, "1")
-    }
-
-    private fun second() {
-        third()
-        Log.v(TAG, "2")
-        fourth()
-    }
-
-    private fun third() {
-        Log.v(TAG, "3")
-    }
-
-    private fun fourth() {
-        Log.v(TAG, "4")
     }
 }
